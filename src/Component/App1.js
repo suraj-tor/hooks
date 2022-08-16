a0d93bf04f083f7691bb65791a0d82cea5776d6e
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-08-17 00:44:33 +0530

    implement basic useEffect

--- src/Component/App1.js
+++ src/Component/App1.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const App1 = () => {
+    const [resourceType, setResourceType] = useState("Posts");
+    useEffect(() => {
+        console.log("in useEffect")
+        // the above will be printed only if the resourceType is changes,
+        // else it will not be printe
+    },[resourceType]);
     return (
         <>
             <div className="pt-5">App1</div>
+            <hr />
+            <div>
+                <button onClick={() => setResourceType("Users")}>Users</button>
+                <button onClick={() => setResourceType("Posts")}>Posts</button>
+                <button onClick={() => setResourceType("Comments")}>Comments</button>
+            </div>
+            <h1>{resourceType}</h1>
         </>
     );
 };
